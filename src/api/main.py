5c22c6cef4aaf4c0e97909af847ce3622279805d
Sheldon-Sunil1 <sheldonsunil21@gmail.com>
2025-08-24 18:43:41 +0530

    Create main.py

---
+++ src/api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from src.db.supabase_client import supabase
+
+app = FastAPI(title="FinAgent API", version="0.1")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to FinAgent API 🚀"}
+
+@app.get("/profiles")
+def get_profiles():
+    try:
+        result = supabase.table("profiles").select("*").limit(5).execute()
+        return result.data
+    except Exception as e:
+        return {"error": str(e)}
